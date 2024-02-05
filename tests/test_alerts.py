51977430e01c209253cad0ec4596dfc9b0cc7607
Alexey Telnov <111047550+signacher@users.noreply.github.com>
2024-02-05 19:14:02 +0300

    add alerts tests

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -21,13 +21,18 @@ def test_confirm_alert():
         alerts.assert_confirm_alert()
 
 
-@allure.label('owner', 'Olga Larionova')
+@allure.label('owner', 'Alexey Telnov')
 @allure.feature('Tests DemoQA')
-@allure.title('Подтверждение оповещения с заполнением текстового поля')
+@allure.title('Alert confirmation')
+@allure.tag('Alerts')
+@allure.link('https://demoqa.com/alerts', name="Demo QA")
+@allure.epic('Alerts, Frame & Windows')
+@allure.feature('Alerts')
+@allure.story('Подтверждение оповещения с заполнением текстового поля')
 def test_prompt_alert():
     with allure.step('Открываем страницу с оповещениями'):
         alerts.open()
-    with allure.step('Нажимаем кнопку '):
+    with allure.step('Нажимаем кнопку Click me'):
         alerts.click_btn_with_prompt()
     with allure.step('Вводим значение в текстовое поле'):
         alerts.type_to_alert('ivanivanov')
